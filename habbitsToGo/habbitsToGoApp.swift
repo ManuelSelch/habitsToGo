5598a2d4ba2212f40aa638a6334bf37c565d8e33
Manuel Selch <manuel@selch.de>
2025-12-22 19:37:07 +0100

    Initial Commit

---
+++ habbitsToGo/habbitsToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  habbitsToGoApp.swift
+//  habbitsToGo
+//
+//  Created by Manuel Selch on 22.12.25.
+//
+
+import SwiftUI
+
+@main
+struct habbitsToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
